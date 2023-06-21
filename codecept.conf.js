26afefd9c8e717f021065c2733a3679dd6a9753c
grus4 <inter84ruslan@gmail.com>
2023-06-21 13:01:58 +0300

    AS project

---
+++ codecept.conf.js
@@ -0,0 +1,61 @@
+const { setHeadlessWhen, setCommonPlugins } = require("@codeceptjs/configure");
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+/** @type {CodeceptJS.MainConfig} */
+exports.config = {
+  tests: "./tests/**/*.js",
+  output: "./output",
+  helpers: {
+    Puppeteer: {
+      //url: 'https://storefront:Ashley2022@staging-na04-ashleystewart.demandware.net/s/AshleyStewart/home/',
+      url: "https://storefront:Ashley2022@dev.ashleystewart.com/",
+      show: true,
+      windowSize: "1920x1080",
+    },
+  },
+  include: {
+    I: "./steps_file.js",
+
+    productLandingPage: "./pages/productLandingPage.js",
+
+    homePage: "./pages/homePage",
+
+    productDetailsPage: "./pages/productDetailsPage",
+
+    shoppingBagPage: "./pages/shoppingBagPage.js",
+
+    yourInformationPage: "./pages/yourInformationPage.js",
+
+    shippingPage: "./pages/shippingPage.js",
+
+    paymentPage: "./pages/paymentPage.js",
+
+    confirmationPage: "./pages/confirmationPage.js",
+
+    shippingAddressGuestUser: "./userData/userData.js"
+  },
+  name: "AS",
+  plugins: {
+    allure: {
+      enabled: false,
+      require: "@codeceptjs/allure-legacy",
+    },
+
+    testomatio: {
+      enabled: false,
+      require: "@testomatio/reporter/lib/adapter/codecept",
+      apiKey: process.env.TESTOMATIO,
+    },
+
+    pauseOnFail: {},
+
+    autoDelay: {
+      enabled: true,
+    },
+  },
+};
